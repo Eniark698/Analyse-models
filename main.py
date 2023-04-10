0d53074b821af637041bc8c7974533f852c282a9
Nazarii Mozol <nmozol@berta.com.ua>
2023-04-10 16:20:27 +0300

    done

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import pandas as pd
 from os import mkdir, listdir, getpid, stat, remove
 
+
 list=listdir('./files/')
-print(list)
 
 
 df = pd.DataFrame()
 
+
 for i in list:
     df1= pd.read_csv('./files/{}'.format(i))
+    df1=df1.drop(columns=['Report Name','Page Name','Selection Pane Name','QueryRefType'])
     df=pd.concat([df, df1])
 
-print(df)
 
-print(df.groupby(['Table']))
+
+
+
+df=df.groupby(['Table','Col/Measure'])['Data Element Count'].count()
+df=pd.DataFrame(df)
+df=df.sort_values('Data Element Count', ascending=False)
+
+
+#.sort_values(by='Data Element Count', ascending=False)
+df.to_csv('./RESULT.csv',encoding='utf-16', sep='\t')
+
