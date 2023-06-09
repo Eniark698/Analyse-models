17a7c4b26f8229ea0dcf51268187f8f404f40fe6
Nazarii Mozol <nmozol@berta.com.ua>
2023-06-09 12:38:44 +0300

    V1.0a
    minor column name changes

--- merge.py
+++ merge.py
@@ -18,7 +18,7 @@ def merge():
     l=len(Columns_table)
     Columns_table = Columns_table.drop(index=[0,1,l-1])
     Columns_table=pd.DataFrame(Columns_table)
-    Columns_table=Columns_table.rename(columns={'IsRowNumber':'Table[Column]'})
+    Columns_table=Columns_table.rename(columns={'IsRowNumber':"'Table'[Column]"})
 
 
 
@@ -29,15 +29,15 @@ def merge():
 
     Measure_table['Concat']="'"+ Measure_table['Table']+"'[" + Measure_table['Name'] + ']'
     Measure_table=Measure_table.drop(columns=['Table', 'Name'])
-    Measure_table=Measure_table.rename(columns={'Concat':'Table[Column]'})
+    Measure_table=Measure_table.rename(columns={'Concat':"'Table'[Column]"})
 
 
     result_model=pd.concat([Columns_table,Measure_table],ignore_index=True)
     result_model =pd.DataFrame(result_model)
     result_model=result_model.drop_duplicates()
 
-    result_model['Table[Column]'].str.strip()
-    result_model=result_model.sort_values(['Table[Column]'], ascending=True)
+    result_model["'Table'[Column]"].str.strip()
+    result_model=result_model.sort_values(["'Table'[Column]"], ascending=True)
 
     del Columns_table,Measure_table
  
@@ -50,11 +50,11 @@ def merge():
 
     result_reports['Concat']="'"+ result_reports['Table']+"'[" + result_reports['Col/Measure'] + ']'
     result_reports=result_reports.drop(columns=['Table', 'Col/Measure'])
-    result_reports=result_reports.rename(columns={'Concat':'Table[Column]'})
+    result_reports=result_reports.rename(columns={'Concat':"'Table'[Column]"})
 
     del Combined_csv
    
-    result_reports['Table[Column]'].str.strip()
+    result_reports["'Table'[Column]"].str.strip()
     
 
 
@@ -63,8 +63,8 @@ def merge():
     
 
 
-    InJOIN=result_model.merge(result_reports, on=['Table[Column]'], how='inner').sort_values(by=['Table[Column]']).drop_duplicates()
-    NotInJOIN=result_model[result_model['Table[Column]'].isin(InJOIN['Table[Column]'])==False]
+    InJOIN=result_model.merge(result_reports, on=["'Table'[Column]"], how='inner').sort_values(by=["'Table'[Column]"]).drop_duplicates()
+    NotInJOIN=result_model[result_model["'Table'[Column]"].isin(InJOIN["'Table'[Column]"])==False]
 
     InJOIN.to_excel(current_working_directory+'/output/InJOIN.xlsx', index=False)
     NotInJOIN.to_excel(current_working_directory+'/output/NotInJOIN.xlsx',index=False)
